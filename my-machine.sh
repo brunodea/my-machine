c4d740bcc8f3d630d75d3f67fc7e65f7a7843883
Bruno Romero de Azevedo <brunordea@gmail.com>
2017-10-14 02:09:28 -0300

    Use  in the PATH.

--- my-machine.sh
+++ my-machine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: ./my-machine <arch_iso>
+# Usage: VBOX=<path_to_vbox_bin> my-machine <arch_iso>
 
 ARCH_ISO_PATH=$1
 if [ -z $ARCH_ISO_PATH ]; then
@@ -8,10 +8,17 @@ if [ -z $ARCH_ISO_PATH ]; then
 	exit 1
 fi
 
+if [ -z $VBOX ]; then
+	echo "Path to the bin files of Virtual Box was not set."
+	exit 1
+fi
+
+PATH="$PATH:$VBOX"
+
 function exit_if_exists() {
 	name=$1
 	type=$2
-	res=$(./VBoxManage list $type | grep $name) 
+	res=$(VBoxManage list $type | grep $name) 
 	if [ ! -z $res ]; then
 		echo "$type '$name' already exists. Remove/Rename it before proceeding."
 		exit 1
@@ -31,34 +38,34 @@ RAM=4096 # 4GB
 VRAM=128 
 
 # Create dynamic disk
-./VBoxManage createhd --filename $DISK_NAME --size $DISK_SIZE 
-./VBoxManage createvm --name $VM_NAME --ostype $OS_TYPE --register
+VBoxManage createhd --filename $DISK_NAME --size $DISK_SIZE 
+VBoxManage createvm --name $VM_NAME --ostype $OS_TYPE --register
 
 # SATA controller with the dynamic disk attached
-./VBoxManage storagectl $VM_NAME --name "SATA Controller" --add sata -- controller IntelAHCI
-./VBoxManage storageattach $VM_NAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $DISK_NAME
+VBoxManage storagectl $VM_NAME --name "SATA Controller" --add sata -- controller IntelAHCI
+VBoxManage storageattach $VM_NAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $DISK_NAME
 
 # IDE controller to attach the install ISO.
-./VBoxManage storagectl $VM_NAME --name "IDE Controller" --add ide
-./VBoxManage storageattach $VM_NAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium $ARCH_ISO_PATH
+VBoxManage storagectl $VM_NAME --name "IDE Controller" --add ide
+VBoxManage storageattach $VM_NAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium $ARCH_ISO_PATH
 
-./VBoxManage modifyvm $VM_NAME --ioapic on
+VBoxManage modifyvm $VM_NAME --ioapic on
 # First boot has to be via DVD, later it should be changed to hdd: TODO.
-./VBoxManage modifyvm $VM_NAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
-./VBoxManage modifyvm $VM_NAME --memory $RAM --vram $VRAM 
-./VBoxManage modifyvm $VM_NAME --natnet default
+VBoxManage modifyvm $VM_NAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
+VBoxManage modifyvm $VM_NAME --memory $RAM --vram $VRAM 
+VBoxManage modifyvm $VM_NAME --natnet default
 
-./VBoxHeadless -s $VM_NAME
+VBoxHeadless -s $VM_NAME
 
 # TODO: configure the OS
 
 #eject the DVD
-#./VBoxManage storageattach $VM_NAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium none
+#VBoxManage storageattach $VM_NAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium none
 
 FIRST_SNAPSHOT_NAME="my-machine-setup"
 # Snapshot after the setup is done.
-./VBoxManage snapshot $VM_NAME take $FIRST_SNAPSHOT_NAME
+VBoxManage snapshot $VM_NAME take $FIRST_SNAPSHOT_NAME
 
 # To get back to a snapshot
-#./VBoxManage snapshot $VM_NAME restore $FIRST_SNAPSHOT_NAME
+#VBoxManage snapshot $VM_NAME restore $FIRST_SNAPSHOT_NAME
 
