ea6bc08979494296b7e4cf11b8addf7efbfe4aed
Bruno Romero de Azevedo <brunordea@gmail.com>
2017-10-29 13:52:17 -0200

    Remove the need for 'expect' when setting up SSH in the guest.

--- setup-arch-ssh.sh
+++ setup-arch-ssh.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 IP=$1
-ROOT_PSW=$2
+ROOT_PWD=$2
 
 NET_INTERFACE="$(ifconfig -s | awk 'NR==3{print $1}')"
 ifconfig "$NET_INTERFACE" "$IP" netmask 255.255.255.0 up
-spawn passwd
-expect "New password:"
-send "$ROOT_PSW\n"
-expect "Retype new password:"
-send "$ROOT_PSW\n"
-
+echo "root:${ROOT_PWD}" | chgpasswd
 sed -i 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config
 systemctl start sshd.service
