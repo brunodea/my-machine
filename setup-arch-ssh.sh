ec898e110615abcc58c625eefa2d320e76d9f0d5
Bruno Romero de Azevedo <brunordea@gmail.com>
2017-11-04 19:44:00 -0200

    Fix setting root's password in archiso.

--- setup-arch-ssh.sh
+++ setup-arch-ssh.sh
@@ -5,6 +5,6 @@ ROOT_PWD=$2
 
 NET_INTERFACE="$(ifconfig -s | awk 'NR==3{print $1}')"
 ifconfig "$NET_INTERFACE" "$IP" netmask 255.255.255.0 up
-echo "root:${ROOT_PWD}" | chgpasswd
+echo -e "${ROOT_PWD}\n${ROOT_PWD}" | passwd root
 sed -i 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config
 systemctl start sshd.service
