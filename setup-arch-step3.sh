ab8fd721387937829aceb223b9c6a070d9f132ad
Bruno Romero de Azevedo <brunordea@gmail.com>
2017-11-06 20:46:56 -0200

    Install XORG and VBoxAdditions after it.
    We should install VBoxAdditions only in the last moment, because it may
    require some of the stuff that will be installed in order to fully
    install it.
    
    That's the case with XORG: VBoxAdditions look for video drivers and
    install things accordingly. If there are none, it won't install
    anything.

--- setup-arch-step3.sh
+++ setup-arch-step3.sh
@@ -10,26 +10,37 @@ echo "Initializing NetworkManager service..."
 # Start NetworkManager service at every boot.
 systemctl enable --now NetworkManager.service
 
+function install_pkg() {
+	pacman -S ${@:1} --noconfirm
+}
+
 # fix Failed to start Load Kernel Modules.
-pacman -S linux-headers --noconfirm
-pacman -S sudo --noconfirm
+install_pkg linux-headers
+install_pkg sudo
 
 ##################################################
 # Make wheel group a sudoer.
 ##################################################
+echo "Setting up user..."
 cp /etc/sudoers .
 wheel_su="%wheel ALL=(ALL) ALL"
 sed -i "s|# $wheel_su|$wheel_su|" sudoers
 visudo -c -f sudoers
 cp sudoers /etc/sudoers
-##################################################
-
-echo "Setting up user..."
 # Create user and add to the wheel group.
 useradd -m -G wheel $USER
 echo -e "${USER_PWD}\n${USER_PWD}" | passwd $USER
+##################################################
+
+##################################################
+# Configure graphics.
+##################################################
+# Using XORG because XFCE currently doesn't support Wayland.
+echo "Configuring graphics..."
+install_pkg xorg
+##################################################
 
 echo "Installing VBoxAdditions..."
-pacman -S virtualbox-guest-utils --noconfirm
+install_pkg virtualboxvirtualbox-guest-utils
 
 echo "========== STEP 3 SUCCESS =========="
