edbd8403c7b5f6cf2069db2a3ede5fe2d7fc2671
Bruno Romero de Azevedo <brunordea@gmail.com>
2017-11-07 02:08:51 -0200

    Install some packages and add some TODOs.

--- setup-arch-step3.sh
+++ setup-arch-step3.sh
@@ -45,6 +45,11 @@ install_pkg xfce4
 sess="session=/usr/bin"
 sed -i "s|# $sess|$sess|" /etc/lxdm/lxdm.conf
 sed -i "s|$sess/startlxde|$sess/startxfce4|" /etc/lxdm/lxdm.conf
+# Configure XFCE.
+install_pkg xfce4-whiskermenu-plugin
+# Install applications
+install_pkg ristretto #image viewer
+install_pkg git
 ##################################################
 
 echo "Installing VBoxAdditions..."
@@ -53,5 +58,14 @@ systemctl enable vboxservice.service
 # Only enable the DM at the end so it doesn't "get in the way".
 # Also, it should only be enabled after installing a Desktop Environment.
 systemctl enable lxdm
+
+# TODO: set vbox property for "reboot finish" in the HOOK provided by LXDM.
+
+# TODO: when configuring stuff, do it as $USER instead of root.
+# TODO: in xfce4-panel.xml replace /home to /home/$USER via sed.
+# TODO: in xfce4-desktop.xml replace /home to /home/$USER via sed.
+# TODO: move default_wallpaper to /home/$USER/wallpapers
+# TODO: download general-cfgs and make the proper soft links
+# TODO: install alsamixer
 
 echo "========== STEP 3 SUCCESS =========="
