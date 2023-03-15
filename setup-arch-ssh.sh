a1a7f3de592fda1c8e3846a4651df7930423ca04
Bruno Romero de Azevedo <brunordea@gmail.com>
2023-03-16 00:31:52 +0100

    Make setup-arch-ssh.sh use `ip` instead of `ifconfig`
    `ifconfig` is deprecated in favor of `ip`.

--- setup-arch-ssh.sh
+++ setup-arch-ssh.sh
@@ -3,8 +3,23 @@
 IP=$1
 ROOT_PWD=$2
 
-NET_INTERFACE="$(ifconfig -s | awk 'NR==3{print $1}')"
-ifconfig "$NET_INTERFACE" "$IP" netmask 255.255.255.0 up
+echo "Adding IP address ${IP}..."
+
+ip_base="$(echo $IP | cut -d'.' -f1-3)"
+
+echo "ip_base: ${ip_base}"
+
+NET_INTERFACE="$(ip -o addr show | grep $ip_base | awk -F\  'NR==1{print $2}')"
+
+if [ -z $NET_INTERFACE ]
+then
+    echo "Network interface not found! Using the default one..."
+    NET_INTERFACE="enp0s8"
+fi
+
+echo "Network interface: ${NET_INTERFACE}"
+
+ip addr add $IP/24 dev $NET_INTERFACE
+
 echo -e "${ROOT_PWD}\n${ROOT_PWD}" | passwd root
 sed -i 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config
-systemctl start sshd.service
